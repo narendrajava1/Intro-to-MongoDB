7a44b6ee3e4eed301d83aa13609db3fbe117baf7
Ziang Lu <ziangl@alumni.cmu.edu>
2019-01-21 16:23:45 -0800

    Finished MongoDB Aggregation Framework demo

---
+++ notebooks/aggregation_framework_demo1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+MongoDB Aggregation Framework demo1.
+
+$match stage demo
+"""
+
+__author__ = 'Ziang Lu'
+
+import pprint
+
+from pymongo import MongoClient
+
+DB = 'mflix'
+PASSWORD = 'Zest2016!'
+
+conn_uri = 'mongodb://zianglu:' + PASSWORD + '@cluster0-shard-00-00-hanbs.mongodb.net:27017,cluster0-shard-00-01-hanbs.mongodb.net:27017,cluster0-shard-00-02-hanbs.mongodb.net:27017/' + \
+    DB + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+
+cli = MongoClient(conn_uri)
+movies_initial = cli.mflix.movies_initial
+
+pipeline = [
+    {
+        '$match': {'language': 'Mandarin, English'}
+    }  # stage1: 'match' stage
+]
+# Note that we can totally use movies_initial.find() method to filter
+
+for result in movies_initial.aggregate(pipeline):
+    pprint.pprint(result)
--- notebooks/aggregation_framework_demo2.py
+++ notebooks/aggregation_framework_demo2.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """
-MongoDB Aggregation Framework demo.
+MongoDB Aggregation Framework demo2.
+
+$group, $sort and $sortByCount stages demo
 """
 
 __author__ = 'Ziang Lu'
@@ -17,29 +19,29 @@ conn_uri = 'mongodb://zianglu:' + PASSWORD + '@cluster0-shard-00-00-hanbs.mongod
 cli = MongoClient(conn_uri)
 movies_initial = cli.mflix.movies_initial
 
-pipeline = [
-    {
-        '$group': {
-            '_id': {'language': '$language', 'year': '$year'},  # Identifier expression to group by
-            'count': {'$sum': 1}  # Accumulator to apply eggregation
-        }
-    },  # "group" stage
-    {
-        '$sort': {'count': -1}
-    }  # "sort" stage
-]
+# pipeline = [
+#     {
+#         '$group': {
+#             '_id': {'language': '$language'},  # Identifier expression to group by
+#             'count': {'$sum': 1}  # Accumulator to apply aggregation
+#         }
+#     },  # stage1: 'group' stage
+#     {
+#         '$sort': {'count': -1}
+#     }  # stage2: 'sort' stage
+# ]
 # Equivalence in SQL:
-# select language, year, count(*) as count
+# select language, count(*) as count
 # from movies_initial
-# group by language, year
+# group by language
 # order by count;
 
 # The above stages can be combined into one single stage:
-# pipeline = [
-#     {
-#         'sortByCount': {'language': '$language', 'year': '$year'}
-#     }
-# ]
+pipeline = [
+    {
+        '$sortByCount': '$language'
+    }  # stage1: 'sortByCount' stage
+]
 
 for result in movies_initial.aggregate(pipeline):
     print(result)
